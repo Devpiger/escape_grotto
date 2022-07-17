24a8848be4fc777f5a30f2b36ad80586fe605bb0
Devpiger <blogerhong@naver.com>
2022-07-18 01:59:45 +0900

    GitTest
    This is GitTest Script

---
+++ Escape Grotto/Assets/GitTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("This is GitTest Script");
+        Debug.Log("This is GitTest Script");
+        Debug.Log("This is GitTest Script");
+    }
+
+    void Update()
+    {
+        
+    }
+}
